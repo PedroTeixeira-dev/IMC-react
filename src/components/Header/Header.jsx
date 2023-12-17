d27a789ab5aed80c99f1b813275372e818d5ee8f
Pedro Teixeira <pedro15at@gmail.com>
2023-12-17 12:49:07 -0300

    "commit "

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,21 @@
+
 import { useEffect, useState } from "react"
 
-const Header = () => {
+function Header() {
     
     const [seuNome, setSeuNome] = useState('')
-    const [nomePreenchido, setNomePreenchido] = useState(false)
-
 
     useEffect(() =>{
-        const nome = prompt("Digite seu nome")
+
+            setSeuNome(prompt('Digite seu nome'))
         
-        function pegaNome(){            
-            if (nome.length > 3) {
-                setSeuNome(nome)
-                setNomePreenchido(true)
-                console.log(nome)
-            } else {
-                alert("nome invalido")
-            }
-        }
-        if (!nomePreenchido) {
-            pegaNome()
-        }
     }, [])
 
     return (
-        (nomePreenchido && (
             <header className="container">
-            <h1>ola, {seuNome}</h1>
-        </header>
-        ))
-
-    )
+                <h1>ola, {seuNome}</h1>
+            </header>
+        )
 }
 
 export default Header
